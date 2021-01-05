0910d395c055d2bbc7a24dd639a7ff7e1f52d1ab
farlop27 <18122011@tecmor.mx>
2021-01-05 16:02:32 -0600

    Primera version eq a hola mundo

---
+++ main.c
@@ -0,0 +1,13 @@
+/* Programa para sumar n numeros
+ * Hecho por farlop
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("Hola\n");
+	return 0;
+}
+
---
+++ readme.md
@@ -0,0 +1,4 @@
+#Programa para sumar n números
+
+Hecho por Paulo Farid López Ayala
+Enero de 2021
